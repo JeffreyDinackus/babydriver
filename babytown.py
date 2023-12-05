540464799419107da5e32ded19414310adb32492
JeffreyDinackus <102326113+JeffreyDinackus@users.noreply.github.com>
2023-12-05 13:22:00 -0500

    Update babytown.py

--- babytown.py
+++ babytown.py
@@ -3,10 +3,32 @@ driver = webdriver.Chrome()
 from selenium.webdriver.common.by import By
 import time
 
+import threading
+import os
+
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 time.sleep(10)
 b = driver.find_element(By.ID, "bigCookie")
 
-while True:
-  b.click()
+def task1(b):
+  i = 0
+    while i < 500:
+       b.click()
+       i+=1
+
+def task2(b):
+  i = 0
+    while i < 500:
+       b.click()
+       i+=1
+
+t1 = threading.Thread(target=task1, name='t1')
+t2 = threading.Thread(target=task2, name='t2')
+
+
+t1.join()
+t2.join()
+
+  
+  
